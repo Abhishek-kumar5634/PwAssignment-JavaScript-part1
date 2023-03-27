156da1ab88079628355f0996fec9ca4c562897bf
Abhishek kumar <91567251+Abhishek-kumar5634@users.noreply.github.com>
2023-03-27 16:09:59 +0530

    Add files via upload

---
+++ vowelCounter.js
@@ -0,0 +1,17 @@
+const string =document.getElementById("str");
+const btn=document.querySelector(".btn");
+const msg=document.querySelector(".msg");
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const str=string.value;
+    console.log(str);
+    let count=0;
+    for(let i=0;i<str.length;i++) {
+        if(
+            str[i]=="a" || str[i]=="e" || str[i]=="i" || str[i]=="o" || str[i]=="u" ) {
+                count +=1;
+            }
+        }
+        msg.innerText="Number of vowels: " +count;
+    });
